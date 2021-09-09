1889fe0990d2e7769cf6cb532b3da3188cc7716c
bashard001 <bdaseh@gmail.com>
2021-09-09 16:48:27 -0700

    created header styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,33 +3,41 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 
 
-export default function Header(){
+export default function Header() {
     const [loaded] = useFonts({
         Lobster: require("../assets/fonts/Lobster-Regular.ttf")
     })
+    if (!loaded) {
+        return null;
+    }
 
-
-return(
-    <View style={styles.header}>
-    <Text style={styles.textCenter}>Weather Dashboard</Text>
-    <Text style="color: black; font-style: italic;">App by Bashar Daseh</Text>
-</View>
-)
-
+    return (
+        <View style={styles.header}>
+            <Text style={styles.textCenter}>Weather Dashboard</Text>
+            <Text style={styles.h6} >App by Bashar Daseh</Text>
+        </View>
+    )
 }
 
-
-const styles =StyleSheet.create({
+const styles = StyleSheet.create({
     header: {
-        top: 0,
-        padding: 25,
-       
+        paddingTop: 30,
+        paddingBottom: 5,
         color: "#fff",
         backgroundColor: "rgba(0,191,255,.5)",
         marginBottom: 20,
-        alignItems: "center",
+        
     },
     textCenter: {
+        marginTop: 15,
+        alignSelf: "center",
         fontFamily: "Lobster"
+    },
+    h6: {
+        color: "black",
+        fontStyle: "italic",
+        alignSelf: "flex-start",
+        fontSize: 10,
+        marginBottom: 0
     }
 })
